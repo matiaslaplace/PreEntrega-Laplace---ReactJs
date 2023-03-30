d730fbd9068b8478d5a859328bd4e66925fa5006
Matias Laplace <matiaslaplace@hotmail.com>
2023-03-30 19:57:17 -0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./componentes/NavBar/NavBar";
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
+import CartWidget from "./componentes/CartWidget/CartWidget";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <ItemListContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import CartWidget from "../CartWidget/CartWidget";
+
+const NavBar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-primary bg-primary">
+      <a class="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Remeras
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Accesorios
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Gorras
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">
+              Stickers
+            </a>
+          </li>
+        </ul>
+      </div>
+      <CartWidget />
+    </nav>
+  );
+};
+
+export default NavBar;
